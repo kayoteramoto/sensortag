c96b55ce2098e36ba07accde10259f45ff69b874
Kayo Teramoto <kteramoto@palantir.com>
2018-10-24 21:38:14 -0700

    Hook up pushbutton and LED, and write

--- index.js
+++ index.js
@@ -1,26 +1,79 @@
 const SensorTag = require('sensortag');
+const fs = require('fs');
+const Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+const LED = new Gpio(4, 'out'); //use GPIO pin 4 as output
+const pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
 
 // 98:07:2D:26:F9:82
 
-SensorTag.discover(tag => {
-  console.log('Discovered new SensorTag!');
+const originalPBValue = pushButton.readSync();
+var connectedTag;
 
-  tag.on('disconnect', () => {
-    console.log('Disconnected!');
-    process.exit(0);
-  });  
+fs.open("./data.csv", 'a', (err, fd) => {
+  function discoverSensorTag(tag) {
+    console.log('Discovered new SensorTag!');
+    connectedTag = tag;
 
-  tag.connectAndSetUp(() => {
-    console.log('Connected!');
-    tag.enableAccelerometer(() => {
-      tag.notifyAccelerometer(() => {
-        tag.on('accelerometerChange', (x, y, z) => {
-          console.log('Accelerometer reading: x = %d, y = %d, z = %d',
-            x.toFixed(1),
-            y.toFixed(1),
-            z.toFixed(1));
+    tag.on('disconnect', () => {
+      console.log('Disconnected from SensorTag!');
+      LED.writeSync(0); // Turn LED off
+      fs.close(fd);
+    });
+
+    tag.connectAndSetUp(() => {
+      console.log('Connected!');
+      LED.writeSync(1); //turn LED on
+
+      tag.enableAccelerometer(() => {
+        tag.notifyAccelerometer(() => {
+          tag.on('accelerometerChange', (x, y, z) => {
+            const timestamp = new Date().valueOf();
+            fs.write(fd, x + ", " + timestamp + ", accelerometer-x");
+            fs.write(fd, y + ", " + timestamp + ", accelerometer-y");
+            fs.write(fd, z + ", " + timestamp + ", accelerometer-z");
+          });
+        });
+      });
+
+      tag.enableGyroscope(() => {
+        tag.notifyGyroscope(() => {
+          tag.on('gyroscopeChange', (x, y, z) => {
+            const timestamp = new Date().valueOf();
+            fs.write(fd, x + ", " + timestamp + ", gyroscope-x");
+            fs.write(fd, y + ", " + timestamp + ", gyroscope-y");
+            fs.write(fd, z + ", " + timestamp + ", gyroscope-z");
+          });
         });
       });
     });
+  }
+
+  pushButton.watch(function (err, pbValue) { //Watch for hardware interrupts on pushButton GPIO, specify callback function
+    if (err) { //if an error
+      console.error('There was an error', err); //output error message to console
+      return;
+    }
+
+    if (pbValue != originalPBValue) {
+      console.log("Attempt SensorTag Discovery");
+      SensorTag.discover(discoverSensorTag);
+    } else {
+      console.log("Stop SensorTag Discovery");
+      if (connectedTag != null) {
+        connectedTag.disconnect();
+      }
+      SensorTag.stopDiscoverAll(discoverSensorTag);
+    }
   });
 });
+
+function unexportOnClose() { //function to run when exiting program
+  LED.writeSync(0); // Turn LED off
+  LED.unexport(); // Unexport LED GPIO to free resources
+  if (connectedTag != null) {
+    connectedTag.disconnect();
+  }
+  pushButton.unexport(); // Unexport Button GPIO to free resources
+};
+
+process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+c
